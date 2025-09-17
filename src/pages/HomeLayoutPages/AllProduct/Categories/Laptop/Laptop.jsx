160029c5256affce7246b6d9ffbfaa1a4d165400
TAMZID <tamzid2233@gmail.com>
2025-09-17 18:37:58 +0600

    added laptop page and fetch laptop data

--- src/pages/HomeLayoutPages/AllProduct/Categories/Laptop/Laptop.jsx
+++ src/pages/HomeLayoutPages/AllProduct/Categories/Laptop/Laptop.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Laptop = () => {
+    const [laptop, setLaptop] = useState([]);
+    useEffect(() => {
+        fetch("/public/ProductsFakeData.json").then(res =>res.json())
+            .then(result => setLaptop(result));
+    }, [])
+    
     return (
-        <div>
-            <h3>this is Laptop</h3>
+        <div className="bg-amber-50 w-full h-screen">
+            <div className='w-11/12 mx-auto'>
+            {/* Top side */}
+            <div className=" bg-white py-2">
+                <h3 className='text-2xl font-semibold'>All Laptop</h3>
+                </div>
+                <div className="">
+
+                </div>
+        </div>
         </div>
     );
 };
