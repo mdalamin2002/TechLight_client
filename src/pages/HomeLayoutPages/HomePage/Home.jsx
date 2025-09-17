b62eb3c499ac0dd6e35bad60a47a41c76cf5f859
KH.Bidyut <khbidyut31@gmail.com>
2025-09-17 23:19:44 +0600

    add featured section

--- src/pages/HomeLayoutPages/HomePage/Home.jsx
+++ src/pages/HomeLayoutPages/HomePage/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './components/Banner';
 import TopProducts from './components/TopProducts';
+import FeaturedSection from './components/FeaturedSection';
 
 const Home = () => {
     return (
         <div className='w-11/12 mx-auto'>
             <Banner></Banner>
             <TopProducts></TopProducts>
+            <FeaturedSection></FeaturedSection>
         </div>
     );
 };
