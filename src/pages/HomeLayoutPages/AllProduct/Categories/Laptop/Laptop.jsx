add846d409ea5af5c3825f17eff778bf8759e6f3
TAMZID <tamzid2233@gmail.com>
2025-09-17 22:30:43 +0600

    added products page and almost complete

--- src/pages/HomeLayoutPages/AllProduct/Categories/Laptop/Laptop.jsx
+++ src/pages/HomeLayoutPages/AllProduct/Categories/Laptop/Laptop.jsx
@@ -1,28 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import LaptopCard from '../../../../../Components/Cards/LaptopCard/LaptopCard';
+import React, { useEffect, useState } from "react";
+import LaptopCard from "../../../../../Components/Cards/LaptopCard/LaptopCard";
 
 const Laptop = () => {
-    const [laptops, setLaptops] = useState([]);
-    useEffect(() => {
-        fetch("/public/ProductsFakeData.json").then(res =>res.json())
-            .then(result => setLaptops(result));
-    }, [])
+  const [laptops, setLaptops] = useState([]);
+  useEffect(() => {
+    fetch("/public/ProductsFakeData.json")
+      .then((res) => res.json())
+      .then((result) => setLaptops(result));
+  }, []);
 
-    return (
-        <div className="bg-amber-50 w-full min-h-screen">
-            <div className='w-11/12 mx-auto'>
-            {/* Top side */}
-            <div className=" bg-white py-2">
-                <h3 className='text-2xl font-semibold'>All Laptop</h3>
-                </div>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-                    {
-                        laptops.map(laptop=><LaptopCard key={laptop?.id} laptop={laptop}></LaptopCard>)
-                    }
-                </div>
+  return (
+    <div className="w-full min-h-screen">
+      <div className="w-11/12 mx-auto">
+        {/* Top side */}
+        <div className=" bg-white p-3 my-3 rounded-lg">
+          <h3 className="text-2xl font-semibold">All Laptop</h3>
         </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+          {laptops.map((laptop) => (
+            <LaptopCard key={laptop?.id} laptop={laptop}></LaptopCard>
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Laptop;
