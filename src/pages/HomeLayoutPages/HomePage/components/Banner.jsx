b62ac94ea2bd48756d9af7f4a21643fdc72b8d2c
samiacodes <emiliyalamia@gmail.com>
2025-09-17 23:00:41 +0600

    Banner added

--- src/pages/HomeLayoutPages/HomePage/components/Banner.jsx
+++ src/pages/HomeLayoutPages/HomePage/components/Banner.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
 
 const Banner = () => {
-    return (
-        <div>
-            <h1>This is Banner</h1>
+  return (
+    <section className=" py-6">
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-6 items-start">
+        {/* Left - Vertical Navigation (1/4 width on md and up) */}
+        <aside className="md:col-span-1">
+          {/* category nav/menu */}
+          <p>Sidebar</p>
+        </aside>
+
+        {/* Right - Carousel (3/4 width on md and up) */}
+        <div className="md:col-span-3 bg-amber-950">
+          {/* carousel/slider */}
+          <p>Carousel</p>
         </div>
-    );
+      </div>
+    </section>
+  );
 };
 
 export default Banner;
