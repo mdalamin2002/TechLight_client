24165e09977b6e171bd742ac869fc68d6bdc57e2
KH.Bidyut <khbidyut31@gmail.com>
2025-09-17 22:56:20 +0600

    add ToProduct section

--- src/pages/HomeLayoutPages/HomePage/Home.jsx
+++ src/pages/HomeLayoutPages/HomePage/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './components/Banner';
+import TopProducts from './components/TopProducts';
 
 const Home = () => {
     return (
         <div className='w-11/12 mx-auto'>
             <Banner></Banner>
+            <TopProducts></TopProducts>
         </div>
     );
 };
