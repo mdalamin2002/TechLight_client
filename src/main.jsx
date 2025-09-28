f26325f056cb8b806aebd92fd0c3f7e9cbff7402
TAMZID <tamzid2233@gmail.com>
2025-09-29 00:59:45 +0600

    fix import issue

--- src/main.jsx
+++ src/main.jsx
@@ -6,17 +6,14 @@ import MainRoute from './router/MainRoute.jsx';
 import store from './store/store';
 import AuthProvider from './context/AuthContext/AuthProvider';
 import { Provider } from 'react-redux';
-import { SidebarProvider } from './Components/ui/sidebar';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
-      <SidebarProvider>
       <Provider store={store}>
         <RouterProvider router={MainRoute} />
         </Provider>
-        </SidebarProvider>
-    </AuthProvider>
+      </AuthProvider>
   </StrictMode>
 );
 
