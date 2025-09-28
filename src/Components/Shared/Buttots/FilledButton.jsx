63590036b042ef785def28bde165748cb8442e5f
samiacodes <emiliyalamia@gmail.com>
2025-09-28 22:00:28 +0600

    shadcn installed

--- src/Components/Shared/Buttots/FilledButton.jsx
+++ src/Components/Shared/Buttots/FilledButton.jsx
@@ -6,8 +6,8 @@ const FilledButton = ({ children, onClick, className = "" }) => {
       onClick={onClick}
       className={`
         px-5 py-2 cursor-pointer rounded-lg font-medium transition-all duration-300
-        bg-[#4B5BC2] text-white
-        hover:bg-[#3749BB] hover:text-white
+        bg-accent text-accent-foreground
+        hover:bg-primary hover:text-primary-foreground
         ${className}
       `}
     >
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
+import { fileURLToPath } from "url";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
