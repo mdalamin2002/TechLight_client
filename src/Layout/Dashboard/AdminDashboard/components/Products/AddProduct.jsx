fadb1ca01849f8ca2018f77cc75954915a6ffd4a
TAMZID <tamzid2233@gmail.com>
2025-10-10 22:29:24 +0600

    added api for posting product data

--- src/Layout/Dashboard/AdminDashboard/components/Products/AddProduct.jsx
+++ src/Layout/Dashboard/AdminDashboard/components/Products/AddProduct.jsx
@@ -8,14 +8,14 @@ const AddProduct = () => {
 
   const handleProductSubmit = async (productData) => {
     console.log(productData);
-    // try {
-    //   const res = await axiosSecure.post("/api/products", productData);
-    //   if (res.data.insertedId) {
-    //     toast.success("Product added successfully!");
-    //   }
-    // } catch (error) {
-    //   toast.error("Failed to add product");
-    // }
+    try {
+      const res = await axiosSecure.post("/api/products", productData);
+      if (res.data.insertedId) {
+        toast.success("Product added successfully!");
+      }
+    } catch (error) {
+      toast.error("Failed to add product");
+    }
   };
 
   return (
