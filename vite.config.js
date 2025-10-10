de92835e4a923fc028bfa7983eab1e673ed10c45
mdalamin <alaminbinesar@gmail.com>
2025-10-10 18:16:22 +0600

    error fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { fileURLToPath } from "url";
-import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -14,10 +14,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-build: {
+// build: {
 
-    cssMinify: 'esbuild', 
+//     cssMinify: 'esbuild', 
 
-  }
+//   }
 
 });
