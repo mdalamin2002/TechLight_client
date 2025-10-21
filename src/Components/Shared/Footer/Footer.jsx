e1a0bcea377226bf142f0d0e2364c0b1789dd156
mdalamin <alaminbinesar@gmail.com>
2025-10-21 23:44:48 +0600

    update footer Quick link

--- src/Components/Shared/Footer/Footer.jsx
+++ src/Components/Shared/Footer/Footer.jsx
@@ -44,10 +44,11 @@ const Footer = () => {
 
   const supportLinks = [
     { name: "FAQ", path: "/faq" },
-    { name: "Privacy Policy", path: "/privacy" },
-    { name: "Terms of Service", path: "/terms" },
-    { name: "Returns & Refunds", path: "/returns" },
-    { name: "Order Tracking", path: "/tracking" },
+    { name: "Privacy Policy", path: "/privacy_policy" },
+    { name: "Terms of Service", path: "/terms-service" },
+    { name: "Returns & Refunds", path: "/returns-refunds" },
+    { name: "Order Tracking", path: "/order-tracking" },
+    { name: "Warranty", path: "/warranty" },
   ];
 
   const categories = [
